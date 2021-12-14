b5b9bd1bf8a0d65f4dda50eb84b5e5c0781cc439
XieRW <1429382875@qq.com>
2021-12-14 17:33:33 +0800

    feat--init

--- xrw/hello/hello.go
+++ xrw/hello/hello.go
@@ -2,10 +2,10 @@ package main
 
 import "fmt"
 
-func hello(name string) string{
-	return "Hello, "+name
+func hello(name string) string {
+	return "Hello, " + name
 }
 
-func main(){
+func main() {
 	fmt.Println(hello("world"))
 }
