f0d75c8b6c3f94b10b69a559ffbe00801e6dd140
XieRW <1429382875@qq.com>
2021-12-14 18:29:26 +0800

    feat--hello,world

--- xrw/hello/hello.go
+++ xrw/hello/hello.go
@@ -2,10 +2,31 @@ package main
 
 import "fmt"
 
-func hello(name string) string {
-	return "Hello, " + name
+const spanish = "Spanish"
+const helloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const french = "French"
+const frenchHelloPrefix = "Bonjour, "
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = helloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(hello("world"))
+	fmt.Println(Hello("World", ""))
 }
