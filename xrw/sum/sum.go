e2875d54bc8342db2bcf4b504d7370d5b6866388
XieRW <1429382875@qq.com>
2021-12-16 10:39:37 +0800

    feat--数组与切片

--- xrw/sum/sum.go
+++ xrw/sum/sum.go
@@ -6,3 +6,21 @@ func Sum(arr []int) (got int) {
 	}
 	return
 }
+
+func SumAll(numbersToSum ...[]int) (got []int) {
+	for _, numbers := range numbersToSum {
+		got = append(got, Sum(numbers))
+	}
+	return
+}
+
+func SumAllTails(numbersToSum ...[]int) (got []int) {
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			numbers = append(numbers, 0)
+		}
+		tail := numbers[1:]
+		got = append(got, Sum(tail))
+	}
+	return
+}
