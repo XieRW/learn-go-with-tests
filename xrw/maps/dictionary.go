785b24610bfac1f3a4dac070b609aa3a908a119b
XieRW <1429382875@qq.com>
2021-12-16 18:23:25 +0800

    feat--maps

---
+++ xrw/maps/dictionary.go
@@ -0,0 +1,11 @@
+package maps
+
+func Search(dictionary map[string]string, word string) string {
+	return dictionary[word]
+}
+
+type Dictionary map[string]string
+
+func (d Dictionary) Search(word string) string {
+	return d[word]
+}
