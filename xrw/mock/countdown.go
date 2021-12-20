031619866e4362334424d93698116c3d8b673b7e
XieRW <1429382875@qq.com>
2021-12-20 18:02:05 +0800

    feat--mocking

---
+++ xrw/mock/countdown.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const (
+	finalWord      = "Go!"
+	countdownStart = 3
+)
+
+func main() {
+	sleeper := &ConfigurableSleeper{1 * time.Second}
+	Countdown(os.Stdout, sleeper)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
+		fmt.Fprintln(out, i)
+	}
+	sleeper.Sleep()
+	fmt.Fprint(out, finalWord)
+}
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	time.Sleep(c.duration)
+}
