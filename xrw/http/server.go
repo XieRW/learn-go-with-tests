f1089e01fde25415b5a6d3eac7340803dc3280f9
XieRW <1429382875@qq.com>
2021-12-23 18:26:36 +0800

    feat--http

---
+++ xrw/http/server.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type PlayerStore interface {
+	GetPlayerScore(name string) int
+}
+
+type PlayerServer struct {
+	store PlayerStore
+}
+
+type StubPlayerStore struct {
+	scores map[string]int
+}
+
+func (s StubPlayerStore) GetPlayerScore(name string) int {
+	score := s.scores[name]
+	return score
+}
+
+func (p PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		w.WriteHeader(http.StatusAccepted)
+		return
+	}
+
+	player := r.URL.Path[len("/players/"):]
+
+	score := p.store.GetPlayerScore(player)
+
+	if score == 0 {
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+	fmt.Fprintf(w, fmt.Sprintf("%d", p.store.GetPlayerScore(player)))
+}
