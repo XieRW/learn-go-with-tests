153a52897dcc9cd58342037343b6bcf42548df8e
XieRW <1429382875@qq.com>
2021-12-27 23:42:03 +0800

    feat--http

--- xrw/http/server.go
+++ xrw/http/server.go
@@ -23,11 +23,15 @@ func (s StubPlayerStore) GetPlayerScore(name string) int {
 }
 
 func (p PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		w.WriteHeader(http.StatusAccepted)
-		return
+	switch r.Method {
+	case http.MethodPost:
+		p.processWin(w)
+	case http.MethodGet:
+		p.showScore(w, r)
 	}
+}
 
+func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
 	player := r.URL.Path[len("/players/"):]
 
 	score := p.store.GetPlayerScore(player)
@@ -36,5 +40,9 @@ func (p PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusNotFound)
 	}
 
-	fmt.Fprintf(w, fmt.Sprintf("%d", p.store.GetPlayerScore(player)))
+	fmt.Fprint(w, score)
+}
+
+func (p *PlayerServer) processWin(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusAccepted)
 }
