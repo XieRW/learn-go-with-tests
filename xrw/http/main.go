c7c4dd44e27e8faadc7adb234854246ee2191176
XieRW <1429382875@qq.com>
2021-12-29 13:17:34 +0800

    feat--http: http.Handler            通过实现这个接口来创建 web 服务器            用 http.HandlerFunc 把普通函数转化为 http.Handler            把 httptest.NewRecorder 作为一个 ResponseWriter 传进去，这样让你可以监视 handler 发送了什么响应            使用 http.NewRequest 构建对服务器的请求

--- xrw/http/main.go
+++ xrw/http/main.go
@@ -5,24 +5,9 @@ import (
 	"net/http"
 )
 
-func NewInMemoryPlayerStore() *InMemoryPlayerStore {
-	return &InMemoryPlayerStore{map[string]int{}}
-}
-
-type InMemoryPlayerStore struct {
-	store map[string]int
-}
-
-func (i *InMemoryPlayerStore) RecordWin(name string) {
-	i.store[name]++
-}
-
-func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-	return i.store[name]
-}
-
 func main() {
 	server := &PlayerServer{NewInMemoryPlayerStore()}
+
 	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf("could not listen on port 5000 %v", err)
 	}
